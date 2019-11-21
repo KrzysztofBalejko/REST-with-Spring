23af1a060ca65400553d13937e63d7d86658524a
Krzysztof Balejko <chrisbalejko86@gmail.com>
2019-11-21 22:16:30 +0000

    :alembic: Autowired for spring dependency injection

--- src/main/java/com/example/demo/Controller/StudentController.java
+++ src/main/java/com/example/demo/Controller/StudentController.java
@@ -2,6 +2,7 @@ package com.example.demo.Controller;
 
 import com.example.demo.Entity.Student;
 import com.example.demo.Service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
@@ -9,7 +10,8 @@ import java.util.Collection;
 @RestController
 public class StudentController {
 
-    private StudentService studentService = new StudentService();
+    @Autowired
+    private StudentService studentService;
 
     public Collection<Student> getAllStudents(){
         return this.studentService.getAllStudents();
--- src/main/java/com/example/demo/Service/StudentService.java
+++ src/main/java/com/example/demo/Service/StudentService.java
@@ -2,6 +2,7 @@ package com.example.demo.Service;
 
 import com.example.demo.Dao.StudentDao;
 import com.example.demo.Entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -9,6 +10,7 @@ import java.util.Collection;
 @Service
 public class StudentService {
 
+    @Autowired
     private StudentDao studentDao;
 
     public Collection<Student> getAllStudents(){
