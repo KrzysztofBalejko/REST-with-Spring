84b8326a80ffe1b473bfa8c414e7d81aac5f58de
Krzysztof Balejko <chrisbalejko86@gmail.com>
2019-11-21 21:51:41 +0000

    :construction: adding data access, introducing StudentDao class

--- src/main/java/com/example/demo/Dao/StudentDao.java
+++ src/main/java/com/example/demo/Dao/StudentDao.java
@@ -1,4 +1,34 @@
 package com.example.demo.Dao;
 
-public class Student {
+import com.example.demo.Entity.Student;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class StudentDao {
+
+    private static Map<Integer, Student> students;
+
+    static {
+
+        students = new HashMap<Integer, Student>(){
+
+            {
+                put(1, new Student(1, "Krzysztof", "Maths"));
+                put(2, new Student(1, "Marcin", "English"));
+                put(3, new Student(1, "Aga", "Biology"));
+                put(4, new Student(1, "Kasia", "Maths"));
+                put(5, new Student(1, "Fryc", "Maths"));
+                put(6, new Student(1, "Dex", "Latin"));
+            }
+
+        };
+
+    }
+
+    public Collection<Student> getAllStudents(){
+        return this.students.values();
+    };
+
 }
