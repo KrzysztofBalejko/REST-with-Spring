18eda493a0796bdb930afdd28ab4355ec6f5b82c
Krzysztof Balejko <chrisbalejko86@gmail.com>
2019-11-21 21:54:54 +0000

    :construction: adding service layer, introducing StudentService class

--- src/main/java/com/example/demo/Service/StudentService.java
+++ src/main/java/com/example/demo/Service/StudentService.java
@@ -1,4 +1,17 @@
 package com.example.demo.Service;
 
+import com.example.demo.Dao.StudentDao;
+import com.example.demo.Entity.Student;
+
+import java.util.Collection;
+
 public class StudentService {
+
+    private StudentDao studentDao;
+
+    public Collection<Student> getAllStudents(){
+        return this.studentDao.getAllStudents();
+    };
+
+
 }
